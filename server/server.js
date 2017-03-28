86576fbde816914f58cc8149343dca12a534d807
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-03-28 15:02:08 -0700

    tried making server side react work

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-const`` express = require('express')
-const`` app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "../public")))
+
+console.log(path.join(__dirname, "../public"))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+
 })
 
 app.listen(3000, function () {
