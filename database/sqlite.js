3a478b970bf6680b45b39cab07bc0b48a434950f
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-03-28 23:22:49 -0700

    finished making sqlite working with express

--- database/sqlite.js
+++ database/sqlite.js
@@ -1,19 +1,33 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('mydb.db')
 
-db.serialize(function () {
-  db.run('CREATE TABLE IF NOT EXISTS entries (info TEXT)')
-  let stmt = db.prepare('INSERT INTO entries VALUES (?)')
+// db.serialize(function () {
+//   db.run('CREATE TABLE IF NOT EXISTS entries (info TEXT)')
+//   let stmt = db.prepare('INSERT INTO entries VALUES (?)')
 
-  for (let i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i)
-  }
+//   for (let i = 0; i < 10; i++) {
+//     stmt.run('Ipsum ' + i)
+//   }
 
-  stmt.finalize()
+//   stmt.finalize()
 
-  db.each('SELECT rowid AS id, info FROM entries', function (err, row) {
-    console.log(row.id + ': ' + row.info)
-  })
-})
+//   db.each('SELECT rowid AS id, info FROM entries', function (err, row) {
+//     console.log(row.id + ': ' + row.info)
+//   })
+// })
+
+ db.serialize(function () {
+        db.run('CREATE TABLE IF NOT EXISTS entries (user TEXT, title TEXT, entry TEXT, logTime TEXT)')
+        let stmt = db.prepare('INSERT INTO entries (user, title, entry, logTime) VALUES (?, ?, ?, ?)')
+
+        stmt.run(request.body.user, request.body.title, request.body.entry, request.body.logTime)
+
+        stmt.finalize()
+
+        db.each('SELECT user, title, entry, logTime FROM entries', function (err, row) {
+            //if (err) console.log(err)
+            console.log(row.user + " " + row.title + " " + row.entry + " " + row.logTime)
+        })
+    })
 
 db.close()
--- server/server.js
+++ server/server.js
@@ -2,22 +2,39 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
 
+
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db')
 
 
 app.use(express.static(path.join(__dirname, "./../public")))
 app.use(bodyParser.json())
 
-console.log(__dirname)
-
 //post should be able to put entry into DB
 app.post('/entries',(request, response) => {
-    console.log(request.body) //THIS IS THE STUFF WE WANT!!!!
+    //console.log(request.body) //THIS IS THE STUFF WE WANT!!!!
+    
+    db.serialize(function () {
+        db.run('CREATE TABLE IF NOT EXISTS entries (user TEXT, title TEXT, entry TEXT, logTime TEXT)')
+        let stmt = db.prepare('INSERT INTO entries (user, title, entry, logTime) VALUES (?, ?, ?, ?)')
+
+        stmt.run(request.body.user, request.body.title, request.body.entry, request.body.logTime)
+
+        stmt.finalize()
+
+        db.each('SELECT user, title, entry, logTime FROM entries', function (err, row) {
+            //if (err) console.log(err)
+            console.log(row.user + "/" + row.title + "/" + row.entry + "/" + row.logTime)
+        })
+    })
+
+    db.close()
     response.send("POST successful???")
 })
 
 app.listen(3000, function () {
   console.log('listening on port 3000!')
 })
+
+
