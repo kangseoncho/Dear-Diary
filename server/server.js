b744cb3ff09fb06f8006e59ca02f8c2079ad13ed
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-03-28 17:35:02 -0700

    got express to work with react

--- server/server.js
+++ server/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 
-app.use(express.static(path.join(__dirname, "../public")))
 
-console.log(path.join(__dirname, "../public"))
 
-app.get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, "./../public")))
 
-})
+console.log(path.join(__dirname, "./../dist"))
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('listening on port 3000!')
 })
