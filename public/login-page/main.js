540b78f6ef9b216cbf47e418218696bc07354970
Kangseon Cho <kangseon.cho@gmail.com>
2017-04-15 16:46:23 -0700

    implement login page

---
+++ public/login-page/main.js
@@ -0,0 +1,15 @@
+function verify() {
+    axios.post('/verification', {
+
+    }).then((res) => {
+
+    })
+}
+
+function signup() {
+    axios.post('/newuser', {
+        
+    }).then((res) => {
+
+    })
+}
