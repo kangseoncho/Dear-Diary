a783370f1dc00099df3b1a166c3aa5f325054af8
Kangseon Cho <kangseon.cho@gmail.com>
2017-04-16 11:50:52 -0700

    modulization of server file and database file

---
+++ server/database.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const pg = require('pg');
+
+//set DB connection
+//first argument is the database name (should be same as postgres CLI DB name, second parameter is fixed, password doesnt matter)
+const connection = new Sequelize('kangseoncho', 'kangseoncho', 'kangseoncho', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+//create DB Model
+const Entries = connection.define('entries',{
+    user: {type: Sequelize.STRING, validate:{ notNull: true }},
+    password: {type: Sequelize.STRING, validate:{ notNull: true }},
+    title: Sequelize.STRING,
+    logTime: Sequelize.STRING,
+    entry: Sequelize.TEXT
+});
+
+//create DB if one does not exist already
+connection.sync();
+
+module.exports = Entries;
--- server/server.js
+++ server/server.js
@@ -1,35 +1,9 @@
-const Sequelize = require('sequelize');
-const pg = require('pg');
+const Entries = require('./database');
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-
-//set DB connection
-const connection = new Sequelize('kangseoncho', 'kangseoncho', 'blog', {
-  host: 'localhost',
-  dialect: 'postgres',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
-
-//create DB Model
-const Entries = connection.define('entries',{
-    user: Sequelize.STRING,
-    password: Sequelize.STRING,
-    title: Sequelize.STRING,
-    logTime: Sequelize.STRING,
-    entry: Sequelize.TEXT
-});
-
-//create DB if one does not exist already
-connection.sync();
-
 
 app.use(express.static(path.join(__dirname, "./../public/blog")))
 app.use(bodyParser.json())
@@ -39,22 +13,21 @@ app.post('/entries',(request, response) => {
     //console.log("request ", request)
     console.log("I am request.body: ", request.body) //THIS IS THE STUFF WE WANT!!!!
 
-    //input stuff into DB
-    Entries.create({
-        user: request.body.username,
-        password: "test password",
+    //find user and save entry
+    Entries.update({
         title: request.body.title,
         logTime: request.body.logTime,
         entry: request.body.entry
-    }).then((task) => task.save());
+    }, { 
+      where: {user: "test user"} //user should be dynamic. value should come from login
+    }).then((result) => console.log("i am promise after updating user entry: ", result))
 
-    //post to DB using sequelize
     response.json(request.body)
 })
 
 //should make a get request to get the entry based on the title
 app.get('/findEntries', (request, response) => {
-   
+   Entries.findAll().then((result) => response.json(result))
 })
 
 
