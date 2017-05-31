8dd1c5acd4e94924ed68ead1f7633800bd145759
Kangseon Cho <kangseon.cho@gmail.com>
2017-05-30 21:11:28 -0700

    no changes

--- src/app.js
+++ src/app.js
@@ -19,56 +19,55 @@ class App extends Component {
     this.getFromDB = this.getFromDB.bind(this);
   }
 
-//will get the time and date of the diary entry
-logTime () {
-  return moment().format('MMMM Do, YYYY'); //typeof === string
-}
+  //will get the time and date of the diary entry
+  logTime() {
+    return moment().format('MMMM Do, YYYY'); //typeof === string
+  }
 
- //custom function to update state based on entry entries
- updateEntry(event) {
-  this.setState({entry: event.target.value});
- }
+  //custom function to update state based on entry entries
+  updateEntry(event) {
+    this.setState({ entry: event.target.value });
+  }
+  updateTitle(event) {
+    this.setState({ title: event.target.value })
+  }
 
- updateTitle(event) {
-   this.setState({title: event.target.value})
- }
+  //function that will grab current state and make post request
+  postToDB() {
+    axios.post('http://localhost:3000/entries', {
+      user: this.state.user,
+      title: this.state.title,
+      entry: this.state.entry,
+      logTime: this.logTime()
+    }).then((response) => {
+      alert("logged new blog entry");
+      console.log("I am from Axios post request: ", response)
+    })
+  }
 
-//function that will grab current state and make post request
- postToDB () {
-   axios.post('http://localhost:3000/entries', {
-     user: this.state.user,
-     title: this.state.title,
-     entry: this.state.entry,
-     logTime: this.logTime()
-   }).then((response) => {
-     alert("logged new blog entry");
-     console.log("I am from Axios post request: ", response)
-   })
- }
-
-//get entries from DB
-getFromDB() {
-   axios.get('http://localhost:3000/findEntries')
-   .then((response) => {
-     //will fild the object with the title in the seachbox
-       let searchItem = document.getElementById('searchField').value;
-       let result = response.data.filter((element, index) => searchItem === element.title)
+  //get entries from DB
+  getFromDB() {
+    axios.get('http://localhost:3000/findEntries')
+      .then((response) => {
+        //will fild the object with the title in the seachbox
+        let searchItem = document.getElementById('searchField').value;
+        let result = response.data.filter((element, index) => searchItem === element.title)
 
         //populate fields with past information
         this.setState({
-            user: result[0].user,
-            title: result[0].title,
-            entry: result[0].entry
+          user: result[0].user,
+          title: result[0].title,
+          entry: result[0].entry
         })
-     })
-  .catch((err) => console.log(err))
-}
+      })
+      .catch((err) => console.log(err))
+  }
 
   render() {
     return (
       <div className="App">
-        <Book entry = {this.state.entry} user={this.state.user} title={this.state.title} updateTitle={this.updateTitle} logTime={this.logTime()}
-        updateEntry={this.updateEntry} postToDB={this.postToDB} getFromDB={this.getFromDB} />
+        <Book entry={this.state.entry} user={this.state.user} title={this.state.title} updateTitle={this.updateTitle} logTime={this.logTime()}
+          updateEntry={this.updateEntry} postToDB={this.postToDB} getFromDB={this.getFromDB} />
       </div>
     );
   }
