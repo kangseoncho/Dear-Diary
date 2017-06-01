e7367740f298c3d727ee8a4bd9827b7e091b9a2c
Kangseon Cho <kangseon.cho@gmail.com>
2017-05-31 22:10:21 -0700

    changed components into stateless functional

--- src/book.js
+++ src/book.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Page from './page.js';
-import axios from 'axios';
 
-class Book extends Component {
-  render() {
-    // should be able to access a particular entry with a get request.
-    return (
-      <div className="Book">
-        <Page user={this.props.user} updateTitle={this.props.updateTitle} entry = {this.props.entry} title={this.props.title}
-        logTime={this.props.logTime} updateEntry={this.props.updateEntry} postToDB={this.props.postToDB} getFromDB={this.props.getFromDB}/>
+const Book = (props) => {
+  return (
+     <div className="Book">
+        <Page user={props.user} updateTitle={props.updateTitle} entry = {props.entry} title={props.title}
+        logTime={props.logTime} updateEntry={props.updateEntry} postToDB={props.postToDB} getFromDB={props.getFromDB}/>
       </div>
-    );
-  }
+  )
 }
+
 
 export default Book;
--- src/page.js
+++ src/page.js
@@ -1,35 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Page extends Component {
-  render() {
-    //console.log(this.props.entry) // logs the current state and the text entry
-    //console.log(this.props.updateEntry) //logs the function itself
-
-    return (
-      <div className="Page">
+const Page = (props) => {
+  return (
+          <div className="Page">
         <div className="Welcome">
           <br></br>
-          <h1>Welcome to Dear Diary, {this.props.user}</h1>
-          <h4>Entry for the Date: {this.props.logTime}</h4>
-          <h3>Title:   <input type="text" className="currTitle" value={this.props.title} onChange={this.props.updateTitle} />  </h3>
+          <h1>Welcome to Dear Diary, {props.user}</h1>
+          <h4>Entry for the Date: {props.logTime}</h4>
+          <h3>Title:   <input type="text" className="currTitle" value={props.title} onChange={props.updateTitle} />  </h3>
 
             <div>
               <textarea className="Entry" placeholder="Start Writing..." cols="70" wrap="soft" rows="30"
-                  onChange={this.props.updateEntry} value={this.props.entry}>
+                  onChange={props.updateEntry} value={props.entry}>
               </textarea>
             </div>
 
             <div className="log">
-              <div><input type="submit" className="button" value="Log" onClick={this.props.postToDB}/> </div>
+              <div><input type="submit" className="button" value="Log" onClick={props.postToDB}/> </div>
             </div>
             <div className="log">
               <input type="text" id="searchField" />
-              <input type="submit" className="button" value="Get Past Entries" onClick={this.props.getFromDB}/>
+              <input type="submit" className="button" value="Get Past Entries" onClick={props.getFromDB}/>
             </div>
         </div>
       </div>
-    );
-  }
+  )
 }
 
 export default Page;
