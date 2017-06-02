6ceafaee1e7a98a990da3f84a72eba25d1f9857f
Kangseon Cho <kangseon.cho@gmail.com>
2017-06-02 14:11:55 -0700

    input box on title

--- src/book.js
+++ src/book.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import Page from './page.js';
+import Archive from './archive.js';
 
 const Book = (props) => {
+  let archives;
   //should work logic to render all prev entries here
-  return (
-     <div className="Book">
-        <Page user={props.user} updateTitle={props.updateTitle} entry = {props.entry} title={props.title}
-        logTime={props.logTime} updateEntry={props.updateEntry} postToDB={props.postToDB} getFromDB={props.getFromDB}
-        updateSearchDate={props.updateSearchDate} />
+  if (props.archive.length > 0) {
+    archives = props.archive.map((element, index) => {
+      return <Archive key={index} user={props.user} title={element.title} entry={element.entry} title={element.title}
+        logTime={element.logTime} />
+    })
+    return (
+      <div className="Book">
+        {archives}
+        <div>
+          <input type="submit" onClick={props.backToWelcome} value="Back to Create Entry" />
+        </div>
       </div>
-  )
+    )
+  } else {
+    return (
+      <div className="Book">
+        <Page user={props.user} updateTitle={props.updateTitle} entry={props.entry} title={props.title}
+          logTime={props.logTime} updateEntry={props.updateEntry} archive={props.archive}
+          postToDB={props.postToDB} getFromDB={props.getFromDB}
+          updateSearchDate={props.updateSearchDate} getAllFromDB={props.getAllFromDB} />
+      </div>
+    )
+  }
 }
-
 
 export default Book;
